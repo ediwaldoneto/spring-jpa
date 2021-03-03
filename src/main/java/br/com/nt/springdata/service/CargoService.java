54c45ce3cb5ef6af7a8a0b9811e955b53ac885e5
Ediwaldo Neto <ediwaldo.neto@outlook.com>
2021-03-03 10:01:04 -0300

    ajuste atualizar

--- src/main/java/br/com/nt/springdata/service/CargoService.java
+++ src/main/java/br/com/nt/springdata/service/CargoService.java
@@ -2,9 +2,7 @@ package br.com.nt.springdata.service;
 
 import java.util.Optional;
 import java.util.Scanner;
-
 import org.springframework.stereotype.Service;
-
 import br.com.nt.springdata.entities.Cargo;
 import br.com.nt.springdata.repository.CargoRespository;
 
@@ -18,6 +16,7 @@ public class CargoService {
 	}
 
 	public void iniciar(Scanner scanner) {
+		// private static Logger LOGGER = Logger.getLogger("InfoLogging");
 		salvar(scanner);
 	}
 
@@ -46,14 +45,18 @@ public class CargoService {
 		int id = scanner.nextInt();
 		System.out.println("Descricao do cargo");
 		String descricao = scanner.next();
-		System.out.println("Id selecionado :: " + id);
-		System.out.println("Cargo informado :: " + descricao);
-		Cargo cargo = new Cargo();
-		cargo.setId(id);
-		cargo.setDescricao(descricao);
-		cargoRespository.save(cargo);
-		System.out.println("Registro Atualizado");
+		System.out.println("Id selecionado :: " + id + " Cargo informado :: " + descricao);
+		Optional<Cargo> dados = cargoRespository.findById(id);
+		if (dados.isPresent()) {
+			Cargo cargo = new Cargo();
+			cargo.setId(id);
+			cargo.setDescricao(descricao);
+			cargoRespository.save(cargo);
+			System.out.println("Registro Atualizado");
+		} else {
+			System.out.println("Registro nao encontrado");
+		}
+
 	}
-	
-	
+
 }

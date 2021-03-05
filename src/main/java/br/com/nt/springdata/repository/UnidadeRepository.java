e9646588a7da822523015435a10dc543b0b42141
Ediwaldo Neto <ediwaldo.neto@outlook.com>
2021-03-05 09:17:58 -0300

    add entidades funcionario e unidade de trabalho

---
+++ src/main/java/br/com/nt/springdata/repository/UnidadeRepository.java
@@ -0,0 +1,11 @@
+package br.com.nt.springdata.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.nt.springdata.entities.UnidadeTrabalho;
+
+@Repository
+public interface UnidadeRepository extends CrudRepository<UnidadeTrabalho, Integer> {
+
+}

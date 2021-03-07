e9d20643ec6b00cbba75c5d2005da5e2ba258922
Ediwaldo Neto <ediwaldo.neto@outlook.com>
2021-03-07 16:51:25 -0300

    finalização  salvar FuncionarioService

--- src/main/java/br/com/nt/springdata/service/CargoService.java
+++ src/main/java/br/com/nt/springdata/service/CargoService.java
@@ -26,7 +26,7 @@ public class CargoService {
 			System.out.println("  * 1 - Salvar");
 			System.out.println("  * 2 - Deletar");
 			System.out.println("  * 3 - Atualizar");
-			System.out.println("  * 4 - Listar" + "\n");
+			System.out.println("  * 4 - Listar");
 			Integer opcao = scanner.nextInt();
 
 			switch (opcao) {
@@ -63,39 +63,58 @@ public class CargoService {
 	}
 
 	public void salvar(Scanner scanner) {
+
 		System.out.println("Descricao do cargo");
+
 		String descricao = scanner.next();
+
 		Cargo cargo = new Cargo();
 		cargo.setDescricao(descricao);
+
 		cargoRespository.save(cargo);
+
 		System.out.println("Registo Salvo");
 
 	}
 
 	public void deletar(Scanner scanner) {
+
 		System.out.println("ID Cargo");
 		int id = scanner.nextInt();
+
 		Optional<Cargo> descricao = cargoRespository.findById(id);
+
 		System.out.println("Id selecionado :: " + id + " " + descricao.get().getDescricao());
+
 		cargoRespository.deleteById(id);
+
 		System.out.println("Registro deletado");
 
 	}
 
 	public void atualizar(Scanner scanner) {
+
 		System.out.println("ID");
+
 		int id = scanner.nextInt();
+
 		System.out.println("Descricao do cargo");
 		String descricao = scanner.next();
+
 		System.out.println("Id selecionado :: " + id + " Cargo informado :: " + descricao);
 		Optional<Cargo> dados = cargoRespository.findById(id);
+
 		if (dados.isPresent()) {
+
 			Cargo cargo = new Cargo();
 			cargo.setId(id);
 			cargo.setDescricao(descricao);
+
 			cargoRespository.save(cargo);
 			System.out.println("Registro Atualizado");
+
 		} else {
+
 			System.out.println("Registro nao encontrado para atualizacao");
 		}
 
--- src/main/java/br/com/nt/springdata/service/FuncionarioService.java
+++ src/main/java/br/com/nt/springdata/service/FuncionarioService.java
@@ -1,10 +1,15 @@
 package br.com.nt.springdata.service;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 import java.util.Scanner;
 
 import org.springframework.stereotype.Service;
 
+import br.com.nt.springdata.entities.Cargo;
+import br.com.nt.springdata.entities.Funcionario;
+import br.com.nt.springdata.repository.CargoRespository;
 import br.com.nt.springdata.repository.FuncionarioRepository;
 
 @Service
@@ -13,10 +18,12 @@ public class FuncionarioService {
 	private boolean system = true;
 	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	private FuncionarioRepository funcionarioRepository;
+	private CargoRespository cargoRespository;
 
-	public FuncionarioService(FuncionarioRepository funcionarioRepository) {
+	public FuncionarioService(FuncionarioRepository funcionarioRepository, CargoRespository cargoRespository) {
 
 		this.funcionarioRepository = funcionarioRepository;
+		this.cargoRespository = cargoRespository;
 	}
 
 	public void iniciar(Scanner scanner) {
@@ -29,7 +36,7 @@ public class FuncionarioService {
 			System.out.println("  * 1 - Salvar");
 			System.out.println("  * 2 - Deletar");
 			System.out.println("  * 3 - Atualizar");
-			System.out.println("  * 4 - Listar" + "\n");
+			System.out.println("  * 4 - Listar");
 			Integer opcao = scanner.nextInt();
 
 			switch (opcao) {
@@ -60,6 +67,41 @@ public class FuncionarioService {
 	}
 
 	public void salvar(Scanner scanner) {
+
+		System.out.println("Informe o nome");
+		String nome = scanner.next();
+
+		System.out.println("Informe CPF");
+		String cpf = scanner.next();
+
+		System.out.println("Informe salario");
+		double salario = scanner.nextDouble();
+
+		System.out.println("Data da contratacao");
+		String data = scanner.next();
+
+		System.out.println("Cargo ID");
+		Integer id = scanner.nextInt();
+
+		Funcionario funcionario = new Funcionario();
+
+		try {
+
+			funcionario.setNome(nome);
+			funcionario.setCpf(cpf);
+			funcionario.setSalario(salario);
+			funcionario.setDataContratacao(LocalDate.parse(data, formatter));
+			Optional<Cargo> cargo = cargoRespository.findById(id);
+			funcionario.setCargo(cargo.get());
+			funcionarioRepository.save(funcionario);
+
+			System.out.println("Registro");
+
+		} catch (Exception e) {
+
+			System.out.println("ERRO ->" + e.getMessage());
+		}
+
 	}
 
 }

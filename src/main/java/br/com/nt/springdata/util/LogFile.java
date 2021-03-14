e67753c9cbee4a1520472f1e83f1b388fde82bd1
Ediwaldo Neto <ediwaldo.neto@outlook.com>
2021-03-14 20:08:24 -0300

    ajustes logfile

--- src/main/java/br/com/nt/springdata/util/LogFile.java
+++ src/main/java/br/com/nt/springdata/util/LogFile.java
@@ -4,7 +4,16 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Calendar;
 
 public final class LogFile {
@@ -14,13 +23,14 @@ public final class LogFile {
 
 	private static final String ESPACO_EM_BRANCO = " ";
 	private static final String PATH_LOG = "C:\\log\\";
-	private static final String FILE_NAME = "activity.log";
+	private static final String FILE_NAME = "Activity.log";
 
 	public static void writeLog(String info) {
 
 		String dataHora = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(Calendar.getInstance().getTime());
+
 		File file = new File(PATH_LOG);
- 
+
 		if (!file.exists()) {
 			file.mkdir();
 		}
@@ -31,7 +41,6 @@ public final class LogFile {
 
 		try {
 
-			
 			fw = new FileWriter(path, true);
 			bw = new BufferedWriter(fw);
 
@@ -55,4 +64,15 @@ public final class LogFile {
 			}
 		}
 	}
+
+	private static LocalDateTime getCreateTime(File file) throws IOException {
+		Path path = Paths.get(file.getPath());
+		BasicFileAttributeView basicfile = Files.getFileAttributeView(path, BasicFileAttributeView.class,
+				LinkOption.NOFOLLOW_LINKS);
+		BasicFileAttributes attr = basicfile.readAttributes();
+		long date = attr.creationTime().toMillis();
+		Instant instant = Instant.ofEpochMilli(date);
+		return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+	}
+
 }
